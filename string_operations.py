8f24eeceaad03d8b4777aab6954167c1edde8314
karbeto <karbevskk@gmail.com>
2024-10-14 20:28:23 +0200

    More Strings Functions

--- string_operations.py
+++ string_operations.py
@@ -1,2 +1,15 @@
 print("aAbByYzZaA".index("b"))
 print("aAbByYzZaA".index("a"))
+
+my_string = 'abcabc'
+list_string = list(my_string)
+print(list_string)
+print(len(list_string))
+
+sashko = 'yesterdaywasfriday'
+the_list = list(sashko)
+print(the_list[3:6])
+
+print(sashko.isalpha())
+print(sashko.capitalize())
+print(sashko.lower())
